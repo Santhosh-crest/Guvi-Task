9d400dd597e6c4152f5b276f1e5d4a9204233f5b
Santhosh-crest <santhoshsk0203@gmail.com>
2025-04-27 21:18:19 +0530

    Add files via upload

---
+++ Task_4/Question8.java
@@ -0,0 +1,13 @@
+package taskpackage;
+import java.util.*;
+public class Question8 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String a="Welcome to GUVI";
+		for(int i=1;i<=10;i++){
+		    System.out.println(i+"."+a);
+		    
+		}
+	}
+
+}
