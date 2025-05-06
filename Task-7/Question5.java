8434098160fd53d9957571349477b8627ae0bd2e
Santhosh-crest <santhoshsk0203@gmail.com>
2025-05-06 23:01:17 +0530

    Add files via upload

---
+++ Task-7/Question5.java
@@ -0,0 +1,18 @@
+package task7;
+import java.util.*;
+public class Question5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		ArrayList<String> arr=new ArrayList<String>();
+		arr.add("Santhosh");		
+		arr.add("Shyam");
+		arr.add("Kasi");
+		arr.add("Sudharshan");
+		arr.add("Saravana");
+		arr.removeAll(arr);//
+		//arr.clear();
+		System.out.println(arr);
+	}
+
+}
---
+++ Task-7/Question6.java
@@ -0,0 +1,17 @@
+package task7;
+import java.util.*;
+public class Question6 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap<Integer,String> set=new TreeMap<Integer,String>();
+		set.put(300,"Shyam");
+		set.put(100,"mani");
+		set.put(200,"ram");
+		LinkedHashSet<String> set1=new LinkedHashSet<String>();
+		for(int i=0;i<set.size();i++) {
+			set1.addAll(set.values());
+		}
+		System.out.println(set1);
+	}
+
+}
